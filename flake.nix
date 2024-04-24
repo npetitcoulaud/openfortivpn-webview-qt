5efaadc52f4fa8118831a3de57eb40682c02c6c5
Nicolas Petitcoulaud <npetitcoulaud@gmail.com>
2024-04-24 14:36:45 +0200

    cleaning

--- flake.nix
+++ flake.nix
@@ -31,27 +31,3 @@
       };
   };
 }
-
-# with import <nixpkgs> { };
-
-# stdenv.mkDerivation rec {
-#   name = "openfortivpn-webview-qt";
-#   src = pkgs.fetchFromGitHub {
-#     repo = "openfortivpn-webview";
-#     owner = "gm-vm";
-#     rev = "36fd3ea39306152470ab202e9049f282822a0eef";
-#     hash = "sha256-BM5hurJDPYpbt2WV6C1dldLx2wD9eDZlTK/TeHXpmY0=";
-#   };
-
-#   buildInputs = [ pkgs.qt6.qtbase pkgs.qt6.qtwebengine ];
-#   nativeBuildInputs = [ pkgs.qt6.wrapQtAppsHook ];
-#   dontWrapQtApps = true;
-
-#   installPhase = ''
-#     cd openfortivpn-webview-qt
-#     qmake .
-#     make
-#     mkdir -p $out/bin
-#     mv openfortivpn-webview $out/bin/
-#   '';
-# }
